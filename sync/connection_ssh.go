c556d8a5d2a682cc3c37d407c014999e822a3e2d
Markus Blaschke <mblaschke82@gmail.com>
2017-08-09 00:06:22 +0200

    Remove Connection.SshCompressedCommandBuilder

--- sync/connection_ssh.go
+++ sync/connection_ssh.go
@@ -2,23 +2,7 @@ package sync
 
 import (
 	"fmt"
-	"github.com/webdevops/go-shell"
-	"strings"
 )
-
-func (connection *Connection) SshCompressedCommandBuilder(command string, args ...string) []interface{} {
-	originalCmd := []string{
-		shell.Quote(command),
-	}
-
-	for _, val := range args {
-		originalCmd = append(originalCmd, shell.Quote(val))
-	}
-
-	inlineCommand := fmt.Sprintf("%s | gzip --stdout", strings.Join(originalCmd, " "))
-
-	return connection.SshCommandBuilder(inlineCommand)
-}
 
 func (connection *Connection) SshCommandBuilder(command string, args ...string) []interface{} {
 	sshArgs := []string{
