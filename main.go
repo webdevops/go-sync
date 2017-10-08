d0e41c67ed2154a87bd98fb4d4618218bc8202ff
Markus Blaschke <mblaschke82@gmail.com>
2017-10-09 00:07:37 +0200

    Version bump 0.4.0

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ const (
 	// application informations
 	Name    = "gosync"
 	Author  = "webdevops.io"
-	Version = "0.3.0"
+	Version = "0.4.0"
 
 	// self update informations
 	GithubOrganization  = "webdevops"
