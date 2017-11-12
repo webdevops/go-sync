22fbf3d86bc3f1f1f882839f569589f7fb87f2e9
Markus Blaschke <mblaschke82@gmail.com>
2017-11-13 00:35:16 +0100

    Release version 0.5.4

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ const (
 	// application informations
 	Name    = "gosync"
 	Author  = "webdevops.io"
-	Version = "0.5.3"
+	Version = "0.5.4"
 
 	// self update informations
 	GithubOrganization  = "webdevops"
